b050a231b39136669feeacea5af79663717fef62
accngitceo <cris.edward.ocampo@accenture.com>
2025-12-15 11:31:37 +0000

    feat: implement Activities and Leaderboard components with data fetching and modal functionality

--- octofit-tracker/frontend/src/App.jsx
+++ octofit-tracker/frontend/src/App.jsx
@@ -1,1 +1,44 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import Activities from './components/Activities'
+import Leaderboard from './components/Leaderboard'
+import Teams from './components/Teams'
+import Users from './components/Users'
+import Workouts from './components/Workouts'
+import { API_BASE } from './config'
+
+export default function App() {
+  console.log('App using API base:', API_BASE)
+
+  return (
+    <BrowserRouter>
+      <div className="app-container mt-3">
+        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm mb-4">
+          <div className="container-fluid">
+            <NavLink className="navbar-brand" to="/">OctoFit</NavLink>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item"><NavLink className="nav-link" to="/activities">Activities</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" to="/workouts">Workouts</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" to="/teams">Teams</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" to="/users">Users</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" to="/leaderboard">Leaderboard</NavLink></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<div className="card p-4"><h1 className="h3">Welcome to OctoFit Tracker</h1><p>Select a section from the nav.</p></div>} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/workouts" element={<Workouts />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
 // removed: App.jsx is deprecated in favor of App.js
